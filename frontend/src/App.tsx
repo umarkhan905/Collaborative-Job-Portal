696d60833c9ddf8306e2ef42e3b4feec9252b8cf
umarkhan905 <umairzahidkhang@gmail.com>
2025-01-03 09:50:13 +0500

    add UserModal component; update App routing to include CompanyDetailsPage; add tailwind-scrollbar-hide plugin to Tailwind configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import JobsPage from "./pages/JobsPage";
 import CompaniesPage from "./pages/CompaniesPage";
+import CompanyDetailsPage from "./pages/CompanyDetailsPage";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/" element={<Homepage />} />
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/companies" element={<CompaniesPage />} />
+        <Route path="/companies/:id" element={<CompanyDetailsPage />} />
       </Routes>
     </>
   );
